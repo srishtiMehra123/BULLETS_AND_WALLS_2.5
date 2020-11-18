3dabb56afd6814c73384cd6508c8f6a98c57707d
srishtiMehra123 <73944888+srishtiMehra123@users.noreply.github.com>
2020-11-18 11:55:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var wall, thickness
+var bullet , speed,weight 
+
+function setup() {
+  createCanvas(1600,400);
+
+  thickness=random(22,83);
+  speed=random(223,321);
+  weight=random(30,52);
+
+  wall=createSprite(1200, 200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
+
+  bullet=createSprite(50,200,100,10);
+  bullet.velocityX=speed;
+}
+
+function draw() {
+  background(255,255,255);
+  
+  if (hasCollided(bullet,wall)){
+bullet.velocityX=0
+    var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness)
+
+    if (damage>10){
+      bullet.shapeColor=color(255,0,0)
+    }
+
+    if (damage<10){
+      bullet.shapeColor=color(0,255,0)
+    }
+
+  }
+  drawSprites();
+}
+
+function hasCollided(lwall,lbullet){
+
+ 
+
+  if(lwall.x-lbullet.x < lbullet.width+lwall.width){
+      return true
+  }
+       return false; 
+
+}
